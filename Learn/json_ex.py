576cb5b969ee3a3d9f7eeaaee3e63127601c12cf
Pandiarajan Subramanian <pandiarajans@outlook.com>
2020-04-12 16:55:04 -0400

    Adding Gitignore and old pending files are promoted

--- Learn/json_ex.py
+++ Learn/json_ex.py
@@ -1,21 +1,26 @@
+"""Learn how to get JSON format file and read/write JSON."""
 import json
 
-def do_want_to_continue_enter_persion():
-    need_to_add_person = input("Do you want to add a person? ")
-    if( need_to_add_person.lower() == 'yes'):
-        ret_result = True
-    else:
-        ret_result = False
-    return ret_result
+input_file = "C:\\Users\\pandi\\source\\repos\\PyWorks\\test_files\\input.json"
+output_file = "C:\\Users\\pandi\\source\\repos\\PyWorks\\test_files\\output.txt"
 
-    
-while do_want_to_continue_enter_persion() == True:
-    first_name = input("Enter first name: ")
-    last_name = input("Enter last name: ")
-    country = input("Enter nationality: ")
-    zipcode = input("Enter Zipcode: ")
-    person = {'Firstname': first_name, 'Lastname': last_name }
-    person['Address'] = {'Country':country, 'Zipcode':zipcode}
-    person_json = json.dumps(person)
-    print(person_json)
 
+def dump_person_details_in_json():
+    """Dump person details in json."""
+    while input("Do you want to add a person? ").lower() == 'yes':
+        first_name = input("Enter first name: ")
+        last_name = input("Enter last name: ")
+        country = input("Enter nationality: ")
+        zipcode = input("Enter Zipcode: ")
+        person = {'Firstname': first_name, 'Lastname': last_name}
+        person['Address'] = {'Country': country, 'Zipcode': zipcode}
+        person_json = json.dumps(person)
+        print(person_json)
+
+
+def read_json_write_text():
+    """How to read the json file and dumps to text file."""
+    with open(input_file, 'r') as input:
+        input_content = json.load(input)
+        with open(output_file, 'w') as output:
+            output.write(inpuy_content["name"] + "'s  are \n")
